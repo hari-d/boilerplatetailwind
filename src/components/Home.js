474ab78ea8a2445af170261e5e3dafdbe3744f5b
hari-d <hari.deshpande@wmlit.com>
2022-11-07 10:41:26 +0530

    work-hari initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
+      <p>start Writing here</p>
       <p className="text-red-500">Home Page</p>
     </div>
   );
